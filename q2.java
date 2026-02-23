99628271210f5ff587e5c5cc8ecb8ce4fcbf8e47
snehashreedofficial-ux <snehashreedofficial@gmail.com>
2026-02-24 01:09:48 +0530

    q 1,2,3 completed

---
+++ q2.java
@@ -0,0 +1,14 @@
+class q2 {
+    public static void main(String[] args) {
+
+        // Marks
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+
+        // Average calculation
+        double average = (maths + physics + chemistry) / 3.0;
+
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
